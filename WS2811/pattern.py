7921e720b3f9b31fb4506975518cb8f48099986c
Stephen Hitchcock <sghitch@stanford.edu>
2017-02-28 04:16:31 -0800

    Started WS2811 Protocol

--- WS2811/pattern.py
+++ WS2811/pattern.py
@@ -34,28 +34,34 @@ icestick = IceStick()
 icestick.Clock.on()
 for i in range(5):
     icestick.J3[i].output().on()
+icestick.PMOD1[3].output().rename('I').on()
 
 main = icestick.main()
 
-init = [array(*int2seq(ord(c), 8)) for c in 'hello, world  \r\n']
+values = __builtin__.tuple((0,16,0,0,32,0,0,64,0,0,128,0,0,255,0,0))
+#values = __builtin__.tuple((0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+init = [array(*int2seq(v, 8)) for v in values]
 
 printf = Counter(4, ce=True)
 rom = ROM(4, init, printf.O)
 
-data = array(rom.O[7], rom.O[6], rom.O[5], rom.O[4],
-             rom.O[3], rom.O[2], rom.O[1], rom.O[0])
+data = array(rom.O[0], rom.O[1], rom.O[2], rom.O[3],
+             rom.O[4], rom.O[5], rom.O[6], rom.O[7])
 
 clock = Counter(4, cout=True, incr=1)
 baud = clock.COUT
 
-payloadCount = Counter(6, ce=True)
+payloadCount = Counter(8, ce=True)
 payloadCount(ce=baud)
-payloadComp = UGT(6)
-payloadComp(I0=array(1,1,1,0,1,0), I1=payloadCount)
+payloadComp = UGT(8)(I0=array(1,1,1,1,1,1,1,0), I1=payloadCount)
 valid = payloadComp
 
-latch = UGT(6)
-latch(I0=array(0,0,0,1,1,0), I1=payloadCount)
+latchFull = UGT(8)
+latchFull(I0=array(0,0,0,0,0,0,0,1), I1=payloadCount)
+latchSingle = UGT(8)
+latchSingle(I0=array(0,0,0,0,0,0,0,0), I1=payloadCount)
+latchSingleInvert = Not()(latchSingle)
+latch = And2()(latchSingleInvert, latchFull)
 
 count = Counter(3, ce=True, r=True)
 done = Decode(7, 3)(count)
@@ -74,11 +80,9 @@ load = LUT2(I0&~I1)(valid,run)
 shift(1,data,load)
 wire(baud, shift.CE)
 
-signal = And2()
-signal(I0 = shift, I1=latch)
-
-ready = LUT2(~I0 & I1)(run, baud)
-wire(ready, printf.CE)
+readyShift = LUT2(~I0 & I1)(run, baud)
+ready = And2()(readyShift, latch)
+wire(readyShift, printf.CE)
 
 HighVal = array(1,0,0,1)
 LowVal = array(0,0,1,0)
@@ -89,15 +93,19 @@ BitSelect(I0=LowVal, I1=HighVal, S=Select)
 CompVar = UGT(4);
 CompVar(I0=BitSelect.O, I1=clock)
 
+signal = And2()
+signal(I0 = CompVar, I1=latch)
+
 # wire(main.CLKIN, main.J3[0])
 # wire(baud,       main.J3[1])
 # wire(run,        main.J3[2])
 # wire(done,       main.J3[3])
 wire(0,			 main.J3[0])
 wire(baud,       	 main.J3[1])
-wire(shift,       	 main.J3[2])
-wire(latch,      	 main.J3[3])
+wire(valid,       	 main.J3[2])
+wire(shift,      	 main.J3[3])
 wire(signal,      main.J3[4])
+wire(signal,      main.I)
 #wire(ready,      main.J3[5])
 #wire(valid,      main.J3[6])
 #wire(count,      main.J3[4:8])
