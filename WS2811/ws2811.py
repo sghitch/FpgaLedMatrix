c6a5ec1fa60b67bc7edd27aacb4258a766faaa2a
Stephen Hitchcock <sghitch@stanford.edu>
2017-03-01 15:39:17 -0800

    Arbitrary LED quantities support

--- WS2811/ws2811.py
+++ WS2811/ws2811.py
@@ -1,6 +1,7 @@
 import sys
 import __builtin__
 import math
+import colorsys
 from magma import *
 from mantle import *
 from boards.icestick import IceStick
@@ -79,101 +80,56 @@ icestick.PMOD1[3].output().rename('I').on()
 
 main = icestick.main()
 
-# values = __builtin__.tuple((0,16,0,0,32,0,0,64,0,0,128,0,0,255,0,0))
-# #values = __builtin__.tuple((0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-# init = [array(*int2seq(v, 8)) for v in values]
-
-# printf = Counter(4, ce=True)
-# rom = ROM(4, init, printf.O)
-
-# data = array(rom.O[0], rom.O[1], rom.O[2], rom.O[3],
-#              rom.O[4], rom.O[5], rom.O[6], rom.O[7])
-
 clock = Counter(4, cout=True, incr=1)
 baud = clock.COUT
 
-# payloadCount = Counter(8, ce=True)
-# payloadCount(ce=baud)
-# payloadComp = UGT(8)(I0=array(1,1,1,1,1,1,1,0), I1=payloadCount)
-# valid = payloadComp
+num_led = 42
+num_bytes = int(math.pow(2, math.ceil(math.log((num_led * 3), 2)))) - 1
+size = int(math.ceil(math.log(num_bytes + 8, 2))) 
 
-# latchFull = UGT(8)
-# latchFull(I0=array(0,0,0,0,0,0,0,1), I1=payloadCount)
-# latchSingle = UGT(8)
-# latchSingle(I0=array(0,0,0,0,0,0,0,0), I1=payloadCount)
-# latchSingleInvert = Not()(latchSingle)
-# latch = And2()(latchSingleInvert, latchFull)
+colorList = []
+for i in range(num_led):
+    color = colorsys.hls_to_rgb(i * (1.0/50), 0.25, 1)
+    for j in range(3):
+        colorList.append(int(color[j] * 255))
 
-# count = Counter(3, ce=True, r=True)
-# done = Decode(7, 3)(count)
+for i in range(num_bytes - (num_led * 3) + 1):
+    colorList.append(0)
 
-# run = DFF(ce=True)
-# run_n = LUT3([0,0,1,0, 1,0,1,0])
-# run_n(done, valid, run)
-# run(run_n)
-# wire(baud, run.CE)
+#Make sure values contains num_bytes + 1 entries
+values = __builtin__.tuple(colorList)
+#values = __builtin__.tuple((255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0,0,255,128,64,0,0,255,255,0))
+init = [array(*int2seq(v, 8)) for v in values]
+print (len(init))
 
-# reset = LUT2(I0&~I1)(done, run)
-# count(CE=baud, RESET=reset)
+printf = Counter(size-1, ce=True)
+rom = ROM(size-1, init, printf.O)
 
-# shift = PISO(8, ce=True)
-# load = LUT2(I0&~I1)(valid,run)
-# shift(1,data,load)
-# wire(baud, shift.CE)
+data = array(rom.O[0], rom.O[1], rom.O[2], rom.O[3],
+             rom.O[4], rom.O[5], rom.O[6], rom.O[7])
 
-# readyShift = LUT2(~I0 & I1)(run, baud)
-# #ready = And2()(readyShift, latch)
-# wire(readyShift, printf.CE)
+payloadCounter = PAYLOAD_TIMER(size, baud)
+payloadComp = UGT(size)(I0=array(*int2seq(num_bytes, size)), I1=payloadCounter)
+dataMask = DFF(ce=True)(I=payloadComp, ce=baud)
 
-# HighVal = array(1,0,0,1)
-# LowVal = array(0,0,1,0)
-# Select = shift
-# BitSelect = Mux(2,4)
-# BitSelect(I0=LowVal, I1=HighVal, S=Select)
+run = RUN(payloadComp, baud)
+shift = PISO(8, ce=True)
+load = LUT2(I0&~I1)(payloadComp,run)
+shift(1,data,load)
+wire(baud, shift.CE)
 
-# CompVar = UGT(4);
-# CompVar(I0=BitSelect.O, I1=clock)
+readyShift = LUT2(~I0 & I1)(run, baud)
+wire(readyShift, printf.CE)
 
-# signal = And2()
-# signal(I0 = CompVar, I1=latch)
-
-#-------------------------------------------------------------------#
-
-num_led = 5
-size = int(math.ceil(math.log((num_led * 3) + 8, 2)))
-
-test_values = __builtin__.tuple((0,16,0,0,32,0,0,64,0,0,128,0,0,255,0,0))
-#values = __builtin__.tuple((0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-test_init = [array(*int2seq(v, 8)) for v in test_values]
-
-test_printf = Counter(4, ce=True)
-test_rom = ROM(4, test_init, test_printf.O)
-
-test_data = array(test_rom.O[0], test_rom.O[1], test_rom.O[2], test_rom.O[3],
-             test_rom.O[4], test_rom.O[5], test_rom.O[6], test_rom.O[7])
-
-test_payloadCounter = PAYLOAD_TIMER(size, baud)
-test_payloadComp = UGT(size)(I0=array(*int2seq(num_led * 3, size)), I1=test_payloadCounter)
-test_dataMask = DFF(ce=True)(I=test_payloadComp, ce=baud)
-
-test_run = RUN(test_payloadComp, baud)
-test_shift = PISO(8, ce=True)
-test_load = LUT2(I0&~I1)(test_payloadComp,test_run)
-test_shift(1,test_data,test_load)
-wire(baud, test_shift.CE)
-
-test_readyShift = LUT2(~I0 & I1)(test_run, baud)
-wire(test_readyShift, test_printf.CE)
-
-test_signal = WS2811_STREAM(test_shift, test_dataMask, clock)
+signal = WS2811_STREAM(shift, dataMask, clock)
 
 
 wire(0,			            main.J3[0])
 wire(baud,                  main.J3[1])
-wire(test_dataMask,      main.J3[2])
-wire(test_signal,                 main.J3[3])
-wire(0,                main.J3[4])
-wire(test_signal,                main.I)
+wire(dataMask,      main.J3[2])
+wire(run,                 main.J3[3])
+wire(signal,                main.J3[4])
+wire(signal,                main.I)
 
 compile(sys.argv[1], main)
 
